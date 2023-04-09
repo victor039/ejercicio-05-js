339156da04a3aa5a2c76579b807d996721678bdd
victor <vmvg08@hotmail.com>
2023-04-08 23:30:34 -0500

    ejercicio-05-js-openbootcamp

---
+++ ejercicio-05.js
@@ -0,0 +1,11 @@
+let altura = 180;
+let altura_Metro = 1.80;
+let peso_kg = 113;
+let altura_redondeada = Math.ceil(altura_Metro);
+let peso_redondeado = Math.floor(peso_kg);
+let iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+
+
+
+
